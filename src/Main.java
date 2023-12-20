6a8f1742ef909a14bed53c0833f912cd43b5baa0
Lysepten <tlstlsrbrb11@gmail.com>
2023-12-20 11:48:55 +0900

    1단계, 종료기능 구현

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
 	public static void main(String[] args) {
 
-		System.out.println("123");
-		
-	}
+		Scanner sc = new Scanner(System.in);
 
+		new App(sc).run();
+
+		sc.close();
+	}
 }
